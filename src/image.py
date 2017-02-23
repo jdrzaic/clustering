99770a7ba5f02547fb6b76afe241c60562825278
Luka Horvat <darwin226@gmail.com>
2017-02-23 12:09:04 +0100

    Example images

--- src/image.py
+++ src/image.py
@@ -7,17 +7,16 @@ import clustering
 import data_reader
 
 city_block_radius = 5
+k = 3
 
 def test_edge():
-    im = ndimage.imread("../data/plane.jpg", True)
+    im = ndimage.imread("../data/flower.png", True)
 
     sx = ndimage.sobel(im, axis=0, mode='constant')
     sy = ndimage.sobel(im, axis=1, mode='constant')
     sob = np.hypot(sx, sy)
-
-    # Image.fromarray(sob).show()
-
     width, height = sob.shape
+
     pts = np.array([[float(height - y), float(x)] for x in range(width) for y in range(height)]).transpose()
     affinity = np.full((width * height, width * height,), 0.00000000001);
     for x in range(width):
@@ -31,16 +30,17 @@ def test_edge():
                         e = sob[int(round(x + i * t))][int(round(y + j * t))]
                         if e > max_edge: max_edge = e
                     affinity[x * height + y][(x + i) * height + y + j] = 1 / (1 + max_edge)
-    x, res = clustering.process_points(pts, affinity, 2, 0.001)
-    clustering.visualize_result(res, 2, x)
-    # out = np.zeros((width, height))
-    # max_sum = 0
-    # for x in range(width):
-    #     for y in range(height):
-    #         for r in range(width * height):
-    #             out[x][y] += affinity[x * height + y][r]
-    #         if out[x][y] > max_sum: max_sum = out[x][y]
-    # out = out / max_sum * 255;
-    # Image.fromarray(out).show()
+    clusters, res = clustering.process_points(pts, affinity, k, 0.001)
+    # clustering.visualize_result(res, 2, x)
 
-    # print(affinity)
+    rgbs = [(255, 0, 0), (0, 255, 0), (255, 255, 0)]
+    new_im = np.empty((width, height, 3), dtype=np.uint8)
+    for x in range(width):
+        for y in range(height):
+            for i in range(k):
+                if clusters[x * height + y][i] == 1: break
+            r, g, b = rgbs[i]
+            new_im[x][y][0] = (im[x][y] + r) / 2
+            new_im[x][y][1] = (im[x][y] + g) / 2
+            new_im[x][y][2] = (im[x][y] + b) / 2
+    Image.fromarray(new_im).show()
